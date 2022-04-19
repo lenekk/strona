e1f7c9e433e2b199a16a1c723ff91c5fc265bb38
lenekk <48925575+lenekk@users.noreply.github.com>
2022-04-19 22:29:01 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth"
 import 'firebase/firestore'
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyAy3g1yaXcvHkWC9R8s1aRzcIb_Bg5YCzQ",
-    authDomain: "restauracjax-b6e85.firebaseapp.com",
-    projectId: "restauracjax-b6e85",
-    storageBucket: "restauracjax-b6e85.appspot.com",
-    messagingSenderId: "731179182241",
-    appId: "1:731179182241:web:00e5dda50a6c51f5fce8e0"
+    apiKey: "xxxxxxxx",
+    authDomain: "xxxxxxxx",
+    projectId: "xxxxxxxx",
+    storageBucket: "xxxxxxxx",
+    messagingSenderId: "xxxxxxxx",
+    appId: "xxxxxxxx"
 })
 
 
